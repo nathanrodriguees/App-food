cd23b88c268ee84367f90e0010e100d2adefb63a
nathanrodriguees <nathan_rodrigues07@outlook.com>
2024-11-22 14:36:51 -0300

    chore: update dependencies to match Expo version compatibility

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -3,7 +3,7 @@ import { Feather } from '@expo/vector-icons';
 
 export function Search() {
     return (
-        <View className='w-full flex-row border-slate-500 h-14 rounded-full
+        <View className='w-full flex-row border border-slate-500 h-14 rounded-full
         items-center gap-2 px-4 bg-transparent'>
             <Feather name='search' size={24} color='#64748b' />
 
